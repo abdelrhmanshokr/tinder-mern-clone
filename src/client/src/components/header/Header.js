b3212593c60019c3bc6f4cabb9aa4ddf2695bd82
abdelrhmanshokr <abdelrhmanshokr13@gmail.com>
2022-03-28 10:39:51 +0200

    intializing react and node for the frontend and the backend as well, Next up creating the components for the frontend

---
+++ src/client/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+import classes from './Header.module.css';
+
+function Header(){
+    return (
+        <div className={classes.header}> this is header </div>
+    );
+}
+
+export default Header;
